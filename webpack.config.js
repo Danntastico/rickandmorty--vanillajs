f215569f62a55661bfa3e5d30a60196718008779
Danilo <nicolasdpf@gmail.com>
2020-08-28 19:36:12 -0500

    Configured webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+//npm i @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --save-dev
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        user: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: "./public/index.html",
+        filename: "./index.html",
+      },
+    ]),
+  ],
+};
